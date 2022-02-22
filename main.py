ac074f72a1f15fcc62f85e73e69aac04ca76feaf
hu-hamster <hj.325915>
2022-02-22 14:19:47 +0800

    添加了数据处理的逻辑和处理后的数据

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.context import SparkContext
 import pandas as pd
-from submodule.data_process import filter_data
+from submodule.data_process import filter_data, replace_elem
 
 source_file = "data/heart_2020_cleaned.csv"
+save_file = "data/Heart.csv"
+feature_file = "data/feature.csv"
 filter_item = ["HeartDisease", "AlcoholDrinking", "Stroke", "MentalHealth"]
 if __name__ == '__main__':
     # create session
     sc = SparkContext("local")
     spark = SparkSession(sc)
-    filter_data(spark, source_file, filter_item)
+    # Filter out useful information
+    filter_data(spark, source_file, save_file, filter_item)
+    replace_elem(save_file, feature_file)
     # df = spark.read.csv(r"data/heart_2020_cleaned.csv", header=True)
     # df = df.select("HeartDisease", "AlcoholDrinking", "Stroke", "MentalHealth")
     # df.show(10)
--- submodule/data_process.py
+++ submodule/data_process.py
@@ -1,12 +1,67 @@
+import csv
+import string
+
 """
 ** usage: get DataFrame from csv file and isolate useful information
 ** arguments:
 **** spark: SparkSession
 **** read_filename: The path of source file
+**** save_filename: The path of save file
 **** filter_items: filter values, such as: [“name”, "age"]
 """
-def filter_data(spark, read_filename, filter_items):
+def filter_data(spark, read_filename, save_filename, filter_items):
     df = spark.read.csv(read_filename, header=True)
     df = df.select(filter_items)
-    # df = df.map(lambda x:)
+    df.toPandas().to_csv(save_filename)
+    # df.show(10)
+    # df.map(lambda x: change_heartDisease(x))
     # return df
+
+
+"""
+** usage: change element from No or Yes
+** argument: element
+"""
+def change_elem_binary(element):
+    if element == "No":
+        return 0
+    else:
+        return 1
+
+
+"""
+** usage: change MentalHealth element
+** argument: element
+"""
+def change_elem_MentalHealth(element):
+    num = float(element)
+    if 0.0 <= num < 5.0:
+        return 0
+    elif 5.0 <= num < 15.0:
+        return 1
+    else:
+        return 2
+
+
+"""
+** usage: Replace elements in a file with fractions, such as: No - 0, Yes - 1
+** arguments:
+**** sourcefile: The path of source file
+**** savefile: The path of save file
+"""
+def replace_elem(sourcefile, savefile):
+    csv_reader = csv.reader(open(sourcefile))
+    head = []
+    body = []
+    for line in csv_reader:
+        if line[0] == "":
+            head = line[1:]
+            continue
+        for i in range(1, len(line)-1):
+            line[i] = change_elem_binary(line[i])
+        line[len(line)-1] = change_elem_MentalHealth(line[len(line)-1])
+        body.append(line[1:])
+    csv_writer = csv.writer(open(savefile, "w", newline=''))
+    csv_writer.writerow(head)
+    csv_writer.writerows(body)
+
